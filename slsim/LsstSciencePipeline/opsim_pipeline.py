ab34bd6d858e28742a4ccff711d53fca7cd605f5
huangxy256 <xiangyu.huang.1@stonybrook.edu>
2026-07-24 12:01:57 -0400

    fix docstring long url bug

--- slsim/LsstSciencePipeline/opsim_pipeline.py
+++ slsim/LsstSciencePipeline/opsim_pipeline.py
@@ -19,9 +19,9 @@ def opsim_time_series_images_data(
     """Creates time series data from opsim database.
 
     :param ra_list: a list of ra points (in degrees) from objects 
-     we want to collect observations for 
+        we want to collect observations for 
     :param dec_list: a list of dec points (in degrees) from objects we want 
-     to collect observations for 
+        to collect observations for 
     :param opsim_path: string, path to the opsim database. The opsim
         database can be downloaded from
         https://s3df.slac.stanford.edu/data/rubin/sim-data/ under the
